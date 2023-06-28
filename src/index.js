9252ea27ee256f793ddfbd7cf5257030bd557e84
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-06-28 12:09:27 -0400

    updated functionality/ change color of background for fetch 24hr change

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ fetch("https://api.coincap.io/v2/assets")
         console.log(cryptoArray)
 
         cryptoArray.forEach(coin => {
-            coin.imgSource = `https://cryptoicons.org/api/icon/${coin.symbol.toLowerCase()}/200`;
+            coin.imgSource = `assets/images/${coin.symbol.toLowerCase()}.png`;
+            console.log(coin)
             render24HourChange(cryptoArray);
         });
 
@@ -57,7 +58,8 @@ function createCoinMenuItem(symbol, name, price, imgSource) {
 
     const priceElement = document.createElement('p');
     priceElement.className = 'coin-price';
-    priceElement.textContent = `$${price}`;
+    const roundedPrice = Number(price).toFixed(2);
+    priceElement.textContent = `Real-Time Price: $${roundedPrice}`;
 
     const menuImageElement = document.createElement('img');
     menuImageElement.className = 'coin-image';
@@ -84,7 +86,8 @@ function renderCoinDetails(name, symbol, price, imgSource) {
 
     const priceElement = document.createElement('p');
     priceElement.className = 'coin-price';
-    priceElement.textContent = `Real-Time Price: $${price}`;
+    const roundedPrice = Number(price).toFixed(2);
+    priceElement.textContent = `Real-Time Price: $${roundedPrice}`;
 
     const imageElement = document.createElement('img');
     imageElement.src = imgSource;
@@ -96,8 +99,8 @@ function renderCoinDetails(name, symbol, price, imgSource) {
         imageElement.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP7jZUOGvusMIOFUpAGYcEe28KZqFzWIKkB-TIkdSs&s';
     });
 
-    coinDetailsContainer.appendChild(imageElement);
     coinDetailsContainer.appendChild(nameElement);
+    coinDetailsContainer.appendChild(imageElement);
     coinDetailsContainer.appendChild(priceElement);
 
     contentContainer.innerHTML = '';
@@ -124,10 +127,9 @@ function render24HourChange(cryptoArray) {
     const coinChangeContainerElement = document.getElementById('coin-change-container');
     coinChangeContainerElement.innerHTML = '';
 
-    const visibleCoins = cryptoArray.slice(0, 7);
+    const visibleCoins = cryptoArray.slice(0, 10);
 
     visibleCoins.forEach(coin => {
-        console.log(typeof (coin.changePercent24Hr))
         const coinChangeContainer = document.createElement('div');
         coinChangeContainer.className = 'coin-change';
 
@@ -135,14 +137,23 @@ function render24HourChange(cryptoArray) {
         coinNameSymbolElement.textContent = `${coin.name} (${coin.symbol})`;
 
         const coinChangeElement = document.createElement('p');
-        coinChangeElement.textContent = `Percent Change: ${coin.changePercent24Hr}%`;
+        const roundedChange = Math.round(coin.changePercent24Hr * 10) / 10; // Round to the nearest tenth
+        coinChangeElement.textContent = `Percent Change: ${roundedChange}%`;
 
         coinChangeContainer.appendChild(coinNameSymbolElement);
         coinChangeContainer.appendChild(coinChangeElement);
 
+        // Set background color based on the change value
+        if (coin.changePercent24Hr < 0) {
+            coinChangeContainer.style.backgroundColor = 'lightcoral';
+        } else if (coin.changePercent24Hr > 0) {
+            coinChangeContainer.style.backgroundColor = 'lightgreen';
+        }
+
         coinChangeContainerElement.appendChild(coinChangeContainer);
     });
 }
+
 
 
 // Function to go to the previous set of coins
@@ -182,8 +193,23 @@ function handleWatchlistSubmission(cryptoArray) {
             const coin = cryptoArray.find(item => item.symbol.toUpperCase() === symbol);
             if (coin) {
                 const listItem = document.createElement('li');
-                listItem.textContent = `${coin.symbol} - $${coin.priceUsd}`;
-                watchlistItems.appendChild(listItem);
+                const roundedPrice = Number(coin.priceUsd).toFixed(2);
+
+                const listContent = document.createElement('span');
+                listContent.textContent = ` - $${roundedPrice}`;
+
+                const imageElement = document.createElement('img');
+                imageElement.src = coin.imgSource;
+                imageElement.alt = coin.symbol;
+                imageElement.className = 'coin-image';
+
+                listItem.appendChild(imageElement);
+                listItem.appendChild(listContent);
+
+                const listItemContainer = document.createElement('div');
+                listItemContainer.appendChild(listItem);
+                watchlistItems.appendChild(listItemContainer);
+
                 watchlistInput.value = '';
             } else {
                 // Symbol not found in cryptoArray
@@ -192,3 +218,4 @@ function handleWatchlistSubmission(cryptoArray) {
         }
     });
 }
+
