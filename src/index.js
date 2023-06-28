f0a57b64bd09eaade936e006e93a0b0030c99f0c
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-06-28 16:13:32 -0400

    click event listener working for 24hr change container

--- src/index.js
+++ src/index.js
@@ -155,6 +155,12 @@ function render24HourChange(cryptoArray) {
         }
 
         coinChangeContainerElement.appendChild(coinChangeContainer);
+
+
+        coinChangeContainer.addEventListener("click", () => {
+            //console.assert(coin.price != null, "coin.price was null in 24hr handler")
+            renderCoinDetails(coin.name, coin.symbol, coin.priceUsd, coin.imgSource);
+        });
     });
 }
 
